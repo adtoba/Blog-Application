435c437131753c290644921680e521a58fe0e63b
adtoba <adetoba53@gmail.com>
2018-02-06 06:16:22 +0100

    adding files

---
+++ app/src/main/java/com/example/user/blogapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.user.blogapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_app_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Firebase Blog App");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
