c8876e251431815882df04dab4063235ce3f8f56
adtoba <adetoba53@gmail.com>
2018-02-06 15:35:24 +0100

    New

--- app/src/main/java/com/example/user/blogapplication/NewPostActivity.java
+++ app/src/main/java/com/example/user/blogapplication/NewPostActivity.java
@@ -5,13 +5,27 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class NewPostActivity extends AppCompatActivity {
 
     private static int GALLERY_PICK = 1;
-    private ImageButton btn_add_image;
+
+    //Layout and widget references
+    private ImageButton mImageButton;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+    private Button mSubmitButton;
+
+    // Uri
+    private Uri imageUri = null;
+
+    // Other references
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +38,13 @@ public class NewPostActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("New Post");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        btn_add_image = (ImageButton) findViewById(R.id.add_image);
+        // Button references
+        mImageButton = (ImageButton) findViewById(R.id.add_image);
+        mSubmitButton = (Button) findViewById(R.id.btn_submit);
+
+        // Edittext references
+        mPostTitle = (EditText) findViewById(R.id.post_title);
+        mPostDesc = (EditText) findViewById(R.id.post_desc);
 
     }
 
@@ -35,15 +55,29 @@ public class NewPostActivity extends AppCompatActivity {
         startActivityForResult(galleryIntent, GALLERY_PICK);
     }
 
+    public void setBtnSubmitPost(View view) {
+        startPosting();
+    }
+
+    private void startPosting() {
+        String title_text = mPostTitle.getText().toString();
+        String desc_text = mPostDesc.getText().toString();
+
+        if (!TextUtils.isEmpty(title_text) && !TextUtils.isEmpty(desc_text) && imageUri != null) {
+
+        }
+
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == GALLERY_PICK && resultCode == RESULT_OK) {
-            Uri uri = data.getData();
+            imageUri = data.getData();
 
-            btn_add_image.setImageURI(uri);
+            mImageButton.setImageURI(imageUri);
         }
     }
 }
