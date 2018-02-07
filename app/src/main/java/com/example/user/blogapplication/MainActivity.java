74831fc708760af460199434eb3ac5a7273c81b0
adtoba <adetoba53@gmail.com>
2018-02-08 00:52:21 +0100

    New

--- app/src/main/java/com/example/user/blogapplication/MainActivity.java
+++ app/src/main/java/com/example/user/blogapplication/MainActivity.java
@@ -1,31 +1,77 @@
 package com.example.user.blogapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // Toolbar setup
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_app_bar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setTitle("Firebase Blog App");
 
-
+        // Firebase References
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        if (mAuth.getCurrentUser() == null) {
+            sendToSignUp();
+        }
+
+    }
+
+    private static class BlogViewHolder extends RecyclerView.ViewHolder {
+
+        public BlogViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        private void setTitle(String title) {
+
+        }
+
+        private void setDesc(String desc) {
+
+        }
+
+        private void setImage(Context context, String image) {
+
+        }
+    }
+
+    private void sendToSignUp() {
+        Intent signUpIntent = new Intent(this, RegisterActivity.class);
+        signUpIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(signUpIntent);
+    }
+
+    private void sendToLogin() {
+        Intent signInIntent = new Intent(this, LoginActivity.class);
+        signInIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(signInIntent);
+
     }
 
     @Override
@@ -45,27 +91,12 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
+        if (id == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            sendToLogin();
+        }
+
         return super.onOptionsItemSelected(item);
-    }
-
-
-    public void startLoading(View view) {
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
     }
 
 
